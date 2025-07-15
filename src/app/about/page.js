eb9f1291a42a6e494603c951c732baf3e65472a7
kohct1 <kohct2006@gmail.com>
2025-07-16 07:14:07 +0800

    feat: add wrapper to about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,28 +6,31 @@ import Story from "@/components/about/story";
 import Mission from "@/components/about/mission";
 import Stats from "@/components/about/stats";
 import Grid from "@/components/about/grid";
+import Wrapper from "@/components/Wrapper";
 import { motion } from "framer-motion";
 
 export default function About() {
   return (
-    <motion.section
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5, ease: "easeOut" }}
-    >
-      <Navbar />
-      <motion.div
+    <Wrapper>
+      <motion.section
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 0.8, delay: 0.2, ease: "easeOut" }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
       >
-        <HeroAbout />
-        <Story />
-        <Mission />
-        <Stats />
-        <Grid />
-        <Footer />
-      </motion.div>
-    </motion.section>
+        <Navbar />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 0.2, ease: "easeOut" }}
+        >
+          <HeroAbout />
+          <Story />
+          <Mission />
+          <Stats />
+          <Grid />
+          <Footer />
+        </motion.div>
+      </motion.section>
+    </Wrapper>
   );
 }
