dc9ef4114a54b2274c12faac5b3ef21340dc2084
kohct1 <kohct2006@gmail.com>
2025-07-24 21:55:18 +0800

    fix: fix next errors

--- src/components/general/navbar.jsx
+++ src/components/general/navbar.jsx
@@ -114,7 +114,7 @@ const Navbar = ({ user, items, setItems }) => {
                   );
                 } else {
                   return (
-                    <LoginForm>
+                    <LoginForm key={index}>
                       <motion.p
                         initial={{ y: -10, opacity: 0 }}
                         animate={{ y: 0, opacity: 1 }}
