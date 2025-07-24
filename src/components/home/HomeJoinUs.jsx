b634d834656be6c9e7e194ed274a54bec0670e8a
kohct1 <kohct2006@gmail.com>
2025-07-24 11:44:18 +0800

    feat: make home join us section taller

--- src/components/home/HomeJoinUs.jsx
+++ src/components/home/HomeJoinUs.jsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 export default function HomeJoinUs() {
   return (
     <div className="relative z-20">
-      <div className="bg-[#e0f0f9] rounded-3xl relative overflow-hidden aspect-[13/6] z-10 max-sm:aspect-square">
+      <div className="bg-[#e0f0f9] rounded-3xl relative overflow-hidden aspect-[13/6] z-10 max-sm:aspect-[4/5]">
         <div className="w-full h-full flex flex-col justify-center items-center gap-8">
           <div className="flex flex-col relative px-4 gap-[clamp(0px,1vw,16px)] z-10">
             <motion.h1
@@ -42,27 +42,27 @@ export default function HomeJoinUs() {
             className="relative z-10"
           >
             <Link href="/about">
-              <Button className="cursor-pointer">Learn more</Button>
+              <Button className="bg-[#e6b724] cursor-pointer">Learn more</Button>
             </Link>
           </motion.div>
-          <motion.div className="w-3/5 absolute left-[-30%] top-[-55%] rotate-330 aspect-square max-sm:w-2/3 max-sm:left-[-30%] max-sm:top-[-30%]">
+          <motion.div className="w-3/5 absolute left-[-30%] top-[-55%] rotate-330 aspect-square max-sm:w-2/3 max-sm:left-[-30%] max-sm:top-[-20%]">
             <Image src="/images/wipes.png" alt="" fill />
           </motion.div>
-          <div className="w-1/3 absolute left-[-10%] bottom-[-20%] rotate-40 aspect-square max-sm:w-1/2 max-sm:left-[-20%] max-sm:bottom-[-10%]">
+          <div className="w-1/3 absolute left-[-10%] bottom-[-20%] rotate-40 aspect-square max-sm:w-1/2 max-sm:left-[-20%] max-sm:bottom-[-10%] max-sm:rotate-20">
             <Image src="/images/bottles.png" alt="" fill />
           </div>
-          <div className="w-1/2 absolute left-[15%] bottom-[-48%] aspect-square max-sm:w-3/5 max-sm:left-[15%] max-sm:bottom-[-20%]">
+          <div className="w-1/2 absolute left-[15%] bottom-[-48%] aspect-square max-sm:w-3/5 max-sm:left-[15%] max-sm:bottom-[-15%]">
             <Image src="/images/training-cups.png" alt="" fill />
           </div>
-          <div className="w-3/5 absolute right-[-20%] bottom-[-45%] rotate-330 aspect-square max-sm:w-2/3 max-sm:right-[-30%] max-sm:bottom-[-20%]">
+          <div className="w-3/5 absolute right-[-20%] bottom-[-45%] rotate-330 aspect-square max-sm:w-2/3 max-sm:right-[-30%] max-sm:bottom-[-15%]">
             <Image src="/images/crocodile.png" alt="" fill />
           </div>
-          <div className="w-1/2 absolute right-[-20%] top-[-30%] rotate-280 aspect-square max-sm:w-3/5 max-sm:right-[-15%] max-sm:top-[-20%] max-sm:rotate-220">
+          <div className="w-1/2 absolute right-[-20%] top-[-30%] rotate-280 aspect-square max-sm:w-3/5 max-sm:right-[-15%] max-sm:top-[-15%] max-sm:rotate-220">
             <Image src="/images/training-bottles.png" alt="" fill />
           </div>
         </div>
       </div>
-      <div className="w-full bg-[#e6b724] rounded-3xl absolute blur-xl aspect-[13/6] top-0 max-sm:aspect-square"></div>
+      <div className="w-full bg-[#e6b724] rounded-3xl absolute blur-xl aspect-[13/6] top-0 max-sm:aspect-[4/5]"></div>
     </div>
   );
 }
